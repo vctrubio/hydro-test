16fc4915b9e092449d9b23f1f14bf76a88127a57
vctrubio <vctrubio@gmail.com>
2024-06-07 16:40:15 +0200

    calculator v2. still needs graph2

--- app/components/Calculator.jsx
+++ app/components/Calculator.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import '../css/Calculator.css'
 
 
-const CalculatorQuestion = ({ question, min, max, value, setValue }) => {
+const PuntoDeAhorro = () => {
+    return (
+        <flex style={{marginTop: '4em', gap: '4em'}}>
+            <div>
+                Label left
+            </div>
+            <div style={{ width: '350px' }}>
+                <h2>Punto de amortización</h2>
+                <div>
+                    El punto de amortización nos indica un calculo aproximado del tiempo que se tardaría en recuperar la inversión de transitar a Warme, y viene dado por el número de asientos calefactables y la eficiencia energética de cada establecimiento
+                </div>
+            </div>
+        </flex>
+    )
+}
+const CalculatorQuestion = ({ question, min, max, value, setValue, desc }) => {
     const handleSliderChange = (event) => {
         setValue(event.target.value);
     };
@@ -19,13 +34,13 @@ const CalculatorQuestion = ({ question, min, max, value, setValue }) => {
                     onChange={handleSliderChange}
                     className="slider"
                 />
-                <div className='slider-target'>{value}</div>
+                <div className='slider-target'>{value} {desc}</div>
             </div>
         </div>
     );
 };
 
-const BarContainer = ({ title, a, b }) => {
+const BarContainer = ({ title, a, b, footer }) => {
     return (
         <div className='bar-container'>
             <div className='bar-title'>{title}</div>
@@ -33,34 +48,34 @@ const BarContainer = ({ title, a, b }) => {
                 <div className='d-flex flex-column align-end'>
                     <div className='bar-outline' style={{
                         width: '100px', /* Default size */
-                        height: `${a}px`, /* Set height based on value */
-                        backgroundColor: '#4CAF50', /* Green color */
+                        height: `${a.height}px`, /* Set height based on value */
+                        backgroundColor: a.backgroundColor, /* Green color */
                         paddingTop: '5px'
-                    }}>{a}€</div>
-                    <div className='bar-label'>Tradicional</div>
+                    }}>{a.height}€</div>
+                    <div className='bar-label'>{a.label}</div>
                 </div>
                 <div>
                     <div className='bar-outline' style={{
                         width: '100px', /* Default size */
-                        height: `${b}px`, /* Set height based on value */
-                        backgroundColor: '#4C2250', /* Green color */
+                        height: `${b.height}px`, /* Set height based on value */
+                        backgroundColor: b.backgroundColor, /* Green color */
                         paddingTop: '5px'
-                    }}>{b}€</div>
-                    <div className='bar-label'>Warme</div>
+                    }}>{b.height}€</div>
+                    <div className='bar-label'>{b.label}</div>
                 </div>
             </div>
-            <div className='bar-footer'>+21500€ ahorrados (1año)</div>
+            <div className='bar-footer'>{footer}</div>
         </div>
     );
 }
-
 export const Calculator = () => {
     const [questions, setQuestions] = React.useState([
-        { text: 'Cuantas estufas tienes', min: 1, max: 10, value: 1 },
-        { text: 'Cada cuanto cambias la bombona', min: 1, max: 30, value: 1 },
-        { text: 'Cuanto pagas por la luz', min: 1, max: 500, value: 1 },
-        { text: 'Cuanto pagarias por la luz', min: 1, max: 500, value: 1 },
+        { text: 'Cuantas estufas tienes', min: 1, max: 10, value: 4, desc: '', step: 1 },
+        { text: 'Cada cuanto cambias la bombona', min: 1, max: 30, value: 3, desc: 'semanas', step: 1 },
+        { text: 'Cuanto pagas por la luz', min: 1, max: 500, value: 1, desc: '€/mes', step: 10 },
+        { text: 'Cuanto pagarias por la luz', min: 1, max: 500, value: 1, desc: '€/mes', step: 10 },
     ]);
+
 
     const handleValueChange = (index, newValue) => {
         setQuestions(prevQuestions => {
@@ -68,6 +83,30 @@ export const Calculator = () => {
             newQuestions[index].value = newValue;
             return newQuestions;
         });
+    };
+
+    const a = {
+        height: 50,
+        backgroundColor: '#00AF50',
+        label: 'Warme',
+    };
+
+    const b = {
+        height: 90,
+        backgroundColor: '#4C2250',
+        label: 'Tradicional',
+    };
+
+    const c = {
+        height: 120,
+        backgroundColor: '#00AF50',
+        label: 'Warme',
+    };
+
+    const d = {
+        height: 80,
+        backgroundColor: '#4C2250',
+        label: 'Tradicional',
     };
 
     return (
@@ -82,19 +121,19 @@ export const Calculator = () => {
                             min={question.min}
                             max={question.max}
                             value={question.value}
+                            desc={question.desc}
                             setValue={newValue => handleValueChange(index, newValue)}
                         />
                     ))}
                     <p style={{ textAlign: 'left' }}>*Los cálculos se basan en lámparas de calor de 1,5 kWh,<br />
                         quemadores de gas de 12 kWh y un precio de la energía de 3€ por kWh.</p>
                 </div>
-                <div style={{display: 'flex', flexDirection: 'column', gap: 40, margin: '0 4em 0 4em'}}>
-                    <BarContainer title='COMPARATIVA - PRECIO' a={50} b={90} />
-                    <BarContainer title='COMPARATIVA - EMISSIONES' a={120} b={80} />
+                <div style={{ display: 'flex', flexDirection: 'column', gap: 40, margin: '0 4em 0 4em' }}>
+                    <BarContainer title='COMPARATIVA - PRECIO' a={b} b={a} footer={'+21500€ ahorrados (1año)'} />
+                    <BarContainer title='COMPARATIVA - EMISSIONES' a={d} b={c} footer={'+20€ ahorrados (1año)'} />
                 </div>
             </flex>
-            <div className='calculator-chart'>
-            </div>
+            <PuntoDeAhorro />
         </div>
     )
 }
--- storefrontapi.generated.d.ts
+++ storefrontapi.generated.d.ts
@@ -255,77 +255,6 @@ export type SitemapQuery = {
   };
 };
 
-export type FeaturedCollectionFragment = Pick<
-  StorefrontAPI.Collection,
-  'id' | 'title' | 'handle'
-> & {
-  image?: StorefrontAPI.Maybe<
-    Pick<StorefrontAPI.Image, 'id' | 'url' | 'altText' | 'width' | 'height'>
-  >;
-};
-
-export type FeaturedCollectionQueryVariables = StorefrontAPI.Exact<{
-  country?: StorefrontAPI.InputMaybe<StorefrontAPI.CountryCode>;
-  language?: StorefrontAPI.InputMaybe<StorefrontAPI.LanguageCode>;
-}>;
-
-export type FeaturedCollectionQuery = {
-  collections: {
-    nodes: Array<
-      Pick<StorefrontAPI.Collection, 'id' | 'title' | 'handle'> & {
-        image?: StorefrontAPI.Maybe<
-          Pick<
-            StorefrontAPI.Image,
-            'id' | 'url' | 'altText' | 'width' | 'height'
-          >
-        >;
-      }
-    >;
-  };
-};
-
-export type RecommendedProductFragment = Pick<
-  StorefrontAPI.Product,
-  'id' | 'title' | 'handle'
-> & {
-  priceRange: {
-    minVariantPrice: Pick<StorefrontAPI.MoneyV2, 'amount' | 'currencyCode'>;
-  };
-  images: {
-    nodes: Array<
-      Pick<StorefrontAPI.Image, 'id' | 'url' | 'altText' | 'width' | 'height'>
-    >;
-  };
-};
-
-export type RecommendedProductsQueryVariables = StorefrontAPI.Exact<{
-  country?: StorefrontAPI.InputMaybe<StorefrontAPI.CountryCode>;
-  language?: StorefrontAPI.InputMaybe<StorefrontAPI.LanguageCode>;
-}>;
-
-export type RecommendedProductsQuery = {
-  products: {
-    nodes: Array<
-      Pick<StorefrontAPI.Product, 'id' | 'title' | 'handle'> & {
-        priceRange: {
-          minVariantPrice: Pick<
-            StorefrontAPI.MoneyV2,
-            'amount' | 'currencyCode'
-          >;
-        };
-        images: {
-          nodes: Array<
-            Pick<
-              StorefrontAPI.Image,
-              'id' | 'url' | 'altText' | 'width' | 'height'
-            >
-          >;
-        };
-      }
-    >;
-  };
-};
-
 export type PredictiveArticleFragment = {__typename: 'Article'} & Pick<
   StorefrontAPI.Article,
   'id' | 'title' | 'handle' | 'trackingParameters'
@@ -804,6 +733,77 @@ export type PoliciesQuery = {
   };
 };
 
+export type RecommendedProductFragment = Pick<
+  StorefrontAPI.Product,
+  'id' | 'title' | 'handle'
+> & {
+  priceRange: {
+    minVariantPrice: Pick<StorefrontAPI.MoneyV2, 'amount' | 'currencyCode'>;
+  };
+  images: {
+    nodes: Array<
+      Pick<StorefrontAPI.Image, 'id' | 'url' | 'altText' | 'width' | 'height'>
+    >;
+  };
+};
+
+export type RecommendedProductsQueryVariables = StorefrontAPI.Exact<{
+  country?: StorefrontAPI.InputMaybe<StorefrontAPI.CountryCode>;
+  language?: StorefrontAPI.InputMaybe<StorefrontAPI.LanguageCode>;
+}>;
+
+export type RecommendedProductsQuery = {
+  products: {
+    nodes: Array<
+      Pick<StorefrontAPI.Product, 'id' | 'title' | 'handle'> & {
+        priceRange: {
+          minVariantPrice: Pick<
+            StorefrontAPI.MoneyV2,
+            'amount' | 'currencyCode'
+          >;
+        };
+        images: {
+          nodes: Array<
+            Pick<
+              StorefrontAPI.Image,
+              'id' | 'url' | 'altText' | 'width' | 'height'
+            >
+          >;
+        };
+      }
+    >;
+  };
+};
+
+export type FeaturedCollectionFragment = Pick<
+  StorefrontAPI.Collection,
+  'id' | 'title' | 'handle'
+> & {
+  image?: StorefrontAPI.Maybe<
+    Pick<StorefrontAPI.Image, 'id' | 'url' | 'altText' | 'width' | 'height'>
+  >;
+};
+
+export type FeaturedCollectionQueryVariables = StorefrontAPI.Exact<{
+  country?: StorefrontAPI.InputMaybe<StorefrontAPI.CountryCode>;
+  language?: StorefrontAPI.InputMaybe<StorefrontAPI.LanguageCode>;
+}>;
+
+export type FeaturedCollectionQuery = {
+  collections: {
+    nodes: Array<
+      Pick<StorefrontAPI.Collection, 'id' | 'title' | 'handle'> & {
+        image?: StorefrontAPI.Maybe<
+          Pick<
+            StorefrontAPI.Image,
+            'id' | 'url' | 'altText' | 'width' | 'height'
+          >
+        >;
+      }
+    >;
+  };
+};
+
 export type ProductVariantFragment = Pick<
   StorefrontAPI.ProductVariant,
   'availableForSale' | 'id' | 'sku' | 'title'
@@ -1141,14 +1141,6 @@ interface GeneratedQueryTypes {
     return: SitemapQuery;
     variables: SitemapQueryVariables;
   };
-  '#graphql\n  fragment FeaturedCollection on Collection {\n    id\n    title\n    image {\n      id\n      url\n      altText\n      width\n      height\n    }\n    handle\n  }\n  query FeaturedCollection($country: CountryCode, $language: LanguageCode)\n    @inContext(country: $country, language: $language) {\n    collections(first: 1, sortKey: UPDATED_AT, reverse: true) {\n      nodes {\n        ...FeaturedCollection\n      }\n    }\n  }\n': {
-    return: FeaturedCollectionQuery;
-    variables: FeaturedCollectionQueryVariables;
-  };
-  '#graphql\n  fragment RecommendedProduct on Product {\n    id\n    title\n    handle\n    priceRange {\n      minVariantPrice {\n        amount\n        currencyCode\n      }\n    }\n    images(first: 1) {\n      nodes {\n        id\n        url\n        altText\n        width\n        height\n      }\n    }\n  }\n  query RecommendedProducts ($country: CountryCode, $language: LanguageCode)\n    @inContext(country: $country, language: $language) {\n    products(first: 4, sortKey: UPDATED_AT, reverse: true) {\n      nodes {\n        ...RecommendedProduct\n      }\n    }\n  }\n': {
-    return: RecommendedProductsQuery;
-    variables: RecommendedProductsQueryVariables;
-  };
   '#graphql\n  fragment PredictiveArticle on Article {\n    __typename\n    id\n    title\n    handle\n    blog {\n      handle\n    }\n    image {\n      url\n      altText\n      width\n      height\n    }\n    trackingParameters\n  }\n  fragment PredictiveCollection on Collection {\n    __typename\n    id\n    title\n    handle\n    image {\n      url\n      altText\n      width\n      height\n    }\n    trackingParameters\n  }\n  fragment PredictivePage on Page {\n    __typename\n    id\n    title\n    handle\n    trackingParameters\n  }\n  fragment PredictiveProduct on Product {\n    __typename\n    id\n    title\n    handle\n    trackingParameters\n    variants(first: 1) {\n      nodes {\n        id\n        image {\n          url\n          altText\n          width\n          height\n        }\n        price {\n          amount\n          currencyCode\n        }\n      }\n    }\n  }\n  fragment PredictiveQuery on SearchQuerySuggestion {\n    __typename\n    text\n    styledText\n    trackingParameters\n  }\n  query predictiveSearch(\n    $country: CountryCode\n    $language: LanguageCode\n    $limit: Int!\n    $limitScope: PredictiveSearchLimitScope!\n    $searchTerm: String!\n    $types: [PredictiveSearchType!]\n  ) @inContext(country: $country, language: $language) {\n    predictiveSearch(\n      limit: $limit,\n      limitScope: $limitScope,\n      query: $searchTerm,\n      types: $types,\n    ) {\n      articles {\n        ...PredictiveArticle\n      }\n      collections {\n        ...PredictiveCollection\n      }\n      pages {\n        ...PredictivePage\n      }\n      products {\n        ...PredictiveProduct\n      }\n      queries {\n        ...PredictiveQuery\n      }\n    }\n  }\n': {
     return: PredictiveSearchQuery;
     variables: PredictiveSearchQueryVariables;
@@ -1188,6 +1180,14 @@ interface GeneratedQueryTypes {
   '#graphql\n  fragment PolicyItem on ShopPolicy {\n    id\n    title\n    handle\n  }\n  query Policies ($country: CountryCode, $language: LanguageCode)\n    @inContext(country: $country, language: $language) {\n    shop {\n      privacyPolicy {\n        ...PolicyItem\n      }\n      shippingPolicy {\n        ...PolicyItem\n      }\n      termsOfService {\n        ...PolicyItem\n      }\n      refundPolicy {\n        ...PolicyItem\n      }\n      subscriptionPolicy {\n        id\n        title\n        handle\n      }\n    }\n  }\n': {
     return: PoliciesQuery;
     variables: PoliciesQueryVariables;
+  };
+  '#graphql\n  fragment RecommendedProduct on Product {\n    id\n    title\n    handle\n    priceRange {\n      minVariantPrice {\n        amount\n        currencyCode\n      }\n    }\n    images(first: 1) {\n      nodes {\n        id\n        url\n        altText\n        width\n        height\n      }\n    }\n  }\n  query RecommendedProducts ($country: CountryCode, $language: LanguageCode)\n    @inContext(country: $country, language: $language) {\n    products(first: 4, sortKey: UPDATED_AT, reverse: true) {\n      nodes {\n        ...RecommendedProduct\n      }\n    }\n  }\n': {
+    return: RecommendedProductsQuery;
+    variables: RecommendedProductsQueryVariables;
+  };
+  '#graphql\n  fragment FeaturedCollection on Collection {\n    id\n    title\n    image {\n      id\n      url\n      altText\n      width\n      height\n    }\n    handle\n  }\n  query FeaturedCollection($country: CountryCode, $language: LanguageCode)\n    @inContext(country: $country, language: $language) {\n    collections(first: 1, sortKey: UPDATED_AT, reverse: true) {\n      nodes {\n        ...FeaturedCollection\n      }\n    }\n  }\n': {
+    return: FeaturedCollectionQuery;
+    variables: FeaturedCollectionQueryVariables;
   };
   '#graphql\n  query Product(\n    $country: CountryCode\n    $handle: String!\n    $language: LanguageCode\n    $selectedOptions: [SelectedOptionInput!]!\n  ) @inContext(country: $country, language: $language) {\n    product(handle: $handle) {\n      ...Product\n    }\n  }\n  #graphql\n  fragment Product on Product {\n    id\n    title\n    vendor\n    handle\n    descriptionHtml\n    description\n    options {\n      name\n      values\n    }\n    selectedVariant: variantBySelectedOptions(selectedOptions: $selectedOptions, ignoreUnknownOptions: true, caseInsensitiveMatch: true) {\n      ...ProductVariant\n    }\n    variants(first: 1) {\n      nodes {\n        ...ProductVariant\n      }\n    }\n    seo {\n      description\n      title\n    }\n  }\n  #graphql\n  fragment ProductVariant on ProductVariant {\n    availableForSale\n    compareAtPrice {\n      amount\n      currencyCode\n    }\n    id\n    image {\n      __typename\n      id\n      url\n      altText\n      width\n      height\n    }\n    price {\n      amount\n      currencyCode\n    }\n    product {\n      title\n      handle\n    }\n    selectedOptions {\n      name\n      value\n    }\n    sku\n    title\n    unitPrice {\n      amount\n      currencyCode\n    }\n  }\n\n\n': {
     return: ProductQuery;
