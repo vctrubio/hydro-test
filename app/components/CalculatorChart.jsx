9f9e09dfd90d00a7805f240f91baf8f4588704ff
vctrubio <vctrubio@gmail.com>
2024-06-23 13:41:52 +0200

    chart initv2

--- app/components/CalculatorChart.jsx
+++ app/components/CalculatorChart.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceLine } from 'recharts';
 
 export const CalculatorChart = ({ data }) => {
-    const chartData = Array.from({ length: 31 }, (_, index) => {
+    const chartData = Array.from({ length: 13 }, (_, index) => {
+        const x = index - 4;
+        const y = data * x;
 
-        const x = index - 10; // X-axis labels from -10 to 20
-        const y = data * x;   // Example calculation based on x value
-    
         return {
-          name: `${x}`, // X-axis label
-          uv: y,        // Y-axis value based on the calculation
-          fill: y < 0 ? '#ff0110' : '#11119d', // Red for negative values, greenish for positive
+            
+            uv: y,
         };
-      });
-    
-      return (
-        <ResponsiveContainer width="100%" height={400}>
-          <AreaChart data={chartData}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis yAxisId="left" domain={['auto', 'auto']} tick={{ fill: '#666666' }} />
-            <Area type="monotone" dataKey="uv" stroke="#8884d8" fill={chartData.map(entry => entry.fill)} yAxisId="left" />
-          </AreaChart>
-        </ResponsiveContainer>
-      );
-    };
+    });
 
-    
+
+    return (
+        <ResponsiveContainer width="100%" height={400}>
+            <AreaChart data={chartData}>
+                <CartesianGrid stroke="none" />
+                <XAxis dataKey="name" label={{ value: 'Años tras la inversion' }} />
+                <YAxis yAxisId="left" domain={['auto', 'auto']} tick={{ fill: '#666666' }} label={{ value: 'acumulación de ahorro (€)', angle: -90, position: 'insideLeft', style: { fontSize: '18px' }, dx: -6, dy: 40 }} />
+                <Area
+                    type="monotone"
+                    dataKey="uv"
+                    stroke="#000000"
+                    yAxisId="left"
+                />
+            </AreaChart>
+        </ResponsiveContainer>
+    );
+};
