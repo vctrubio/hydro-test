acc19b3aede4f72b0bd2850a50494c0f2b3b0324
vctrubio <vctrubio@gmail.com>
2024-06-23 13:48:34 +0200

    chart colorv3

--- app/components/CalculatorChart.jsx
+++ app/components/CalculatorChart.jsx
@@ -7,11 +7,26 @@ export const CalculatorChart = ({ data }) => {
         const y = data * x;
 
         return {
-            
             uv: y,
+            fill: y < 0 ? '#fa0000' : '#82ca9d', // Red for negative values, greenish for non-negative values
         };
     });
 
+    const gradientOffset = () => {
+        const dataMax = Math.max(...chartData.map((i) => i.uv));
+        const dataMin = Math.min(...chartData.map((i) => i.uv));
+
+        if (dataMax <= 0) {
+            return 0;
+        }
+        if (dataMin >= 0) {
+            return 1;
+        }
+
+        return dataMax / (dataMax - dataMin);
+    };
+
+    const off = gradientOffset();
 
     return (
         <ResponsiveContainer width="100%" height={400}>
@@ -19,12 +34,13 @@ export const CalculatorChart = ({ data }) => {
                 <CartesianGrid stroke="none" />
                 <XAxis dataKey="name" label={{ value: 'Años tras la inversion' }} />
                 <YAxis yAxisId="left" domain={['auto', 'auto']} tick={{ fill: '#666666' }} label={{ value: 'acumulación de ahorro (€)', angle: -90, position: 'insideLeft', style: { fontSize: '18px' }, dx: -6, dy: 40 }} />
-                <Area
-                    type="monotone"
-                    dataKey="uv"
-                    stroke="#000000"
-                    yAxisId="left"
-                />
+                <defs>
+                    <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset={off} stopColor="green" stopOpacity={1} />
+                        <stop offset={off} stopColor="red" stopOpacity={1} />
+                    </linearGradient>
+                </defs>
+                <Area type="monotone" dataKey="uv" stroke="#000" fill="url(#splitColor)" yAxisId="left" />
             </AreaChart>
         </ResponsiveContainer>
     );
