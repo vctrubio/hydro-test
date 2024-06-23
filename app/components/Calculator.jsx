9558e214a6cf7345ab33ce8616e8dacf13ac3010
vctrubio <vctrubio@gmail.com>
2024-06-23 16:12:05 +0200

    calculator reflex

--- app/components/Calculator.jsx
+++ app/components/Calculator.jsx
@@ -5,7 +5,7 @@ import { CalculatorChart } from './CalculatorChart'
 
 const PuntoDeAhorro = ({ data, ahorroMensual }) => {
     return (
-        <div style={{ maxWidth: '989px', alignItems: 'center', marginTop: '5em' }}>
+        <div style={{ maxWidth: '989px', alignItems: 'center', marginTop: '0em'}}>
             <flex className='punto-ahorro'>
                 <div>
                     <h1 >El punto de amortización</h1>
@@ -13,20 +13,8 @@ const PuntoDeAhorro = ({ data, ahorroMensual }) => {
                         El punto de amortización nos indica un calculo aproximado del tiempo que se tardaría en recuperar la inversión de transitar a Warme, y viene dado por el número de asientos calefactables y la eficiencia energética de cada establecimiento.
                     </div>
                 </div>
-                <CalculatorChart data={data} ahorroMensual={ahorroMensual}/>
+                <CalculatorChart data={data} ahorroMensual={ahorroMensual} />
             </flex>
-            <div className='punto-g'>
-                <div className='p-4'>
-                    <h5 className='mb-3'>¿Te gustaría saber exactamente cuánto podrías ahorrar con Warme?</h5>
-                    <p>Déjanos tu mail y te haremos un estudio exhaustivo personalizado</p>
-                    <div className='d-flex flex-row justify-content-center mt-1' style={{ gap: '1em' }}>
-                        <input type='text' placeholder='email' />
-                        <button style={{ width: '200px', fontWeight: 'lighter' }}>Pedir estudio</button>
-                    </div>
-                </div>
-                <div className='punto-g-banner'>
-                </div>
-            </div>
         </div>
     )
 }
@@ -53,13 +41,30 @@ const CalculatorQuestion = ({ question, min, max, value, setValue, desc }) => {
     );
 };
 
-const BarContainer = ({ title, a, b, footer, flag, ahorroMensual, setAhorroMensual}) => {
+const SaberMas = () => {
+    return (
+        <div className='punto-g'>
+            <div className='p-4'>
+                <h5 className='mb-3'>¿Te gustaría saber exactamente cuánto podrías ahorrar con Warme?</h5>
+                <p>Déjanos tu mail y te haremos un estudio exhaustivo personalizado</p>
+                <div className='d-flex flex-row justify-content-center mt-1' style={{ gap: '1em' }}>
+                    <input type='text' placeholder='email' />
+                    <button style={{ width: '200px', fontWeight: 'lighter' }}>Pedir estudio</button>
+                </div>
+            </div>
+            <div className='punto-g-banner'>
+            </div>
+        </div>
+    )
+}
+
+const BarContainer = ({ title, a, b, footer, flag, ahorroMensual, setAhorroMensual }) => {
     // bar-container-height = 400px
     const max = flag === 'huela' ? 18000 : 5600;
     const unit = max / 300;
     const aHeightPx = Math.round(a.height / unit * 10);
     const bHeightPx = Math.round(b.height / unit);
-    
+
     useEffect(() => {
         if (setAhorroMensual) {
             setAhorroMensual(Math.round(b.height - a.height));
@@ -123,7 +128,7 @@ export const Calculator = () => {
     };
 
     const dataLineChart = questions[2].value * 120;
-    
+
     const tradicionalA = {
         height: calculateHuellaCo2(questions, "tradicional"),
         backgroundColor: '#3B5463',
@@ -148,11 +153,12 @@ export const Calculator = () => {
         label: 'Warme',
     };
 
-    return (
+    return (<>
+        <h1 style={{ marginBottom: '2em', textAlign: 'center' }}>Calcula tu ahorro cambiándote a Warme</h1>
+
         <div className='calculator'>
-            <h1 style={{ marginBottom: '2em' }}>Calcula tu ahorro cambiándote a Warme</h1>
             <div className='calculator-mb'>
-                <flex>
+                <div>
                     <div className='calculator-qs'>
                         {questions.map((question, index) => (
                             <CalculatorQuestion
@@ -166,18 +172,25 @@ export const Calculator = () => {
                             />
                         ))}
                     </div>
-                </flex>
+                </div>
                 <div className='calculator-footer'>
                     *Los cálculos se basan en lámparas de calor de 1,5 kWh,<br />
                     quemadores de gas de 12 kWh y un precio de la <br />energía de 3€ por kWh.
                 </div>
+                {/* <SaberMas /> */}
             </div>
             <div className='bar-container-head'>
-                <BarContainer title='Huela Co2' a={warmeA} b={tradicionalA} footer={'KG/Co2 (mes)'} flag="huela" />
-                <BarContainer title='Gastos Economicos' a={warmeB} b={tradicionalB} footer={'€ (mes)'} flag="gastos" ahorroMensual={ahorroMensual} setAhorroMensual={setAhorroMensual} />
+                <div className='d-flex'>
+                    <BarContainer title='Huela Co2' a={warmeA} b={tradicionalA} footer={'KG/Co2 (mes)'} flag="huela" />
+                    <BarContainer title='Gastos Economicos' a={warmeB} b={tradicionalB} footer={'€ (mes)'} flag="gastos" ahorroMensual={ahorroMensual} setAhorroMensual={setAhorroMensual} />
+                </div>
             </div>
-            <PuntoDeAhorro data={dataLineChart} ahorroMensual={ahorroMensual} />
-
         </div>
+        <div className='d-flex flex-column justify-content-center align-items-center'>
+            <PuntoDeAhorro data={dataLineChart} ahorroMensual={ahorroMensual} />
+            <SaberMas />
+        </div>
+
+    </>
     )
 }
