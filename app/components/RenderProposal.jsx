6bfc8d18b986499a32849bba9751a6fd4429e035
vctrubio <vctrubio@gmail.com>
2024-06-23 15:21:28 +0200

    hover for redner

--- app/components/RenderProposal.jsx
+++ app/components/RenderProposal.jsx
@@ -103,6 +103,7 @@ const RenderMenu = ({ data, uiPillo, setUiPillo, selection, setSelection }) => {
 
 const RenderView = ({ selectedColor }) => {
     const mountRef = useRef(null);
+    const [isImageVisible, setIsImageVisible] = useState(false);
 
     const handleResize = useCallback((renderer, camera) => {
         const width = mountRef.current.clientWidth;
@@ -111,6 +112,10 @@ const RenderView = ({ selectedColor }) => {
         camera.aspect = width / height;
         camera.updateProjectionMatrix();
     }, []);
+
+    const handleClick = () => {
+        setIsImageVisible(!isImageVisible);
+    };
 
     useEffect(() => {
         // Scene, camera, and renderer
@@ -212,7 +217,18 @@ const RenderView = ({ selectedColor }) => {
     }, [handleResize, selectedColor]); // Re-run the effect when selectedColor changes
 
     return (
-        <div className="render-view" ref={mountRef} />
+        <div className="render-view" ref={mountRef} style={{ position: 'relative' }}>
+            {isImageVisible ? (
+                <img src={`/path-to-your-image/A.jpg`} alt="A" style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, border: '1px solid black' }} />
+            ) : null}
+            <button onClick={handleClick} style={{ position: 'absolute', top: 0, right: 20, zIndex: 1000, backgroundColor: 'transparent' }}>
+                {isImageVisible ? (
+                    <svg fill="#000000" width="40px" height="40px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M 9 7 C 4.039063 7 0 11.035156 0 16 C 0 20.964844 4.039063 25 9 25 L 23 25 C 27.957031 25 32 20.957031 32 16 C 32 11.042969 27.957031 7 23 7 Z M 23 9 C 26.878906 9 30 12.121094 30 16 C 30 19.878906 26.878906 23 23 23 C 19.121094 23 16 19.878906 16 16 C 16 12.121094 19.121094 9 23 9 Z" /></svg>)
+                    : (
+                        <svg fill="#000000" width="40px" height="40px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M 9 7 C 8.378906 7 7.773438 7.066406 7.1875 7.1875 C 6.894531 7.246094 6.59375 7.320313 6.3125 7.40625 C 3.792969 8.203125 1.742188 10.085938 0.71875 12.5 C 0.605469 12.769531 0.492188 13.03125 0.40625 13.3125 C 0.136719 14.164063 0 15.058594 0 16 C 0 16.929688 0.144531 17.8125 0.40625 18.65625 C 0.410156 18.664063 0.402344 18.679688 0.40625 18.6875 C 1.203125 21.207031 3.085938 23.257813 5.5 24.28125 C 5.769531 24.394531 6.03125 24.507813 6.3125 24.59375 C 7.164063 24.863281 8.058594 25 9 25 L 23 25 C 27.957031 25 32 20.957031 32 16 C 32 11.042969 27.957031 7 23 7 Z M 9 9 C 12.878906 9 16 12.121094 16 16 C 16 19.878906 12.878906 23 9 23 C 5.121094 23 2 19.878906 2 16 C 2 15.757813 2.007813 15.515625 2.03125 15.28125 C 2.386719 11.742188 5.363281 9 9 9 Z M 14.625 9 L 23 9 C 26.878906 9 30 12.121094 30 16 C 30 19.878906 26.878906 23 23 23 L 14.625 23 C 16.675781 21.347656 18 18.828125 18 16 C 18 13.171875 16.675781 10.652344 14.625 9 Z" /></svg>
+                    )}
+            </button>
+        </div>
     );
 };
 
