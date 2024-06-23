68ce86bb26d8042db4133ca92b4a7a20cfce50fc
vctrubio <vctrubio@gmail.com>
2024-06-23 14:04:23 +0200

    save show pitu

--- app/components/CalculatorChart.jsx
+++ app/components/CalculatorChart.jsx
@@ -36,7 +36,7 @@ export const CalculatorChart = ({ data }) => {
                     yAxisId="left"
                     domain={[4000, 80000]}
                     label={{ value: 'acumulación de ahorro (€)', angle: -90, position: 'insideLeft', style: { fontSize: '18px' }, dx: -6, dy: 40 }}
-                    tickFormatter={(value) => value === 0 ? '0' : `${(value / 1000)}k`} // Formats the tick values as "8k" or "0"
+                    tickFormatter={(value) => value === 0 ? '0' : `${(Math.round(value / 1000))}k`} // Formats the tick values as "8k" or "0"
                 />
                 <defs>
                     <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
